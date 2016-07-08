1369a151cdd8a9efc48f21ffae38da7d9f440bf4
Thomas Tourlourat <thomas@tourlourat.com>
2016-07-08 12:36:13 +0200

    First Commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Tapfiliate for PHP
+
+## Included features
+
+1. Create conversion : http://docs.tapfiliate.apiary.io/#reference/conversions/conversions-collection/create-a-conversion
+
+## Example
+
+```php
+$key   = "APP_KEY"; // Generated from : https://tapfiliate.com/user/api-access/
+
+$tapfiliate    = new Tapfiliate($key, $base);
+$indexMember = new MemberIndex($tapfiliate, $table);
+```
+
+```php
+```
+
+## Testing
+
+Not implemented yet.
+
+## License
+
+This library is under the MIT license. [See the complete license](https://github.com/armetiz/tapfiliate-php/blob/master/LICENSE).
+
+## Credits
+
+Author - [Thomas Tourlourat](http://www.wozbe.com)
---
+++ src/Tapfiliate.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace Armetiz\TapfiliateSDK;
+
+use Assert\Assertion;
+use Buzz;
+use Buzz\Message\Response;
+
+/**
+ * @author : Thomas Tourlourat <thomas@tourlourat.com>
+ */
+class Tapfiliate
+{
+    /**
+     * @var Buzz\Browser
+     */
+    private $browser;
+
+    /**
+     * Tapfiliate constructor.
+     *
+     * @param string $key
+     */
+    public function __construct($key)
+    {
+        Assertion::string($key);
+
+        // @see https://github.com/kriswallsmith/Buzz/pull/186
+        $listener = new Buzz\Listener\CallbackListener(function (Buzz\Message\RequestInterface $request, $response = null) use ($key) {
+            if ($response) {
+                // postSend
+            } else {
+                // preSend
+                $request->addHeader(sprintf('Api-Key: %s', $key));
+            }
+        });
+
+        $this->browser = new Buzz\Browser(new Buzz\Client\Curl());
+        $this->browser->addListener($listener);
+    }
+
+    private function guardOptions($options, $constraints)
+    {
+        Assertion::allInArray(array_keys($options), array_keys($constraints));
+
+        foreach ($options as $key => $value) {
+            $constraint = $constraints[$key];
+
+            if (is_callable($constraint)) {
+                call_user_func($constraint, $value);
+            }
+        }
+    }
+
+    public function createConversion($options, $overrideMaxCookieTime = null)
+    {
+        $this->guardOptions($options, [
+            "click"           => function ($value) {
+                Assertion::isArray($value);
+                Assertion::keyExists("id", $value);
+            },
+            "coupon"          => [Assertion::class, "string"],
+            "visitor_id"      => [Assertion::class, "string"],
+            "external_id"     => [Assertion::class, "string"],
+            "amount"          => [Assertion::class, "numeric"],
+            "commission_type" => [Assertion::class, "string"],
+            "commissions"     => function ($value) {
+                Assertion::allKeyExists("sub_amount", $value);
+                Assertion::allKeyExists("commission_type", $value);
+            },
+            "meta_data"       => function ($value) {
+                Assertion::isArray($value);
+            },
+            "program_group"   => [Assertion::class, "string"],
+        ]);
+
+        $parameters = [];
+        if (is_bool($overrideMaxCookieTime)) {
+            $parameters = [
+                "override_max_cookie_time" => $overrideMaxCookieTime,
+            ];
+        }
+
+        /** @var Response $response */
+        $response = $this->browser->post(
+            $this->getEndpoint($parameters),
+            [
+                "content-type" => "application/json",
+            ],
+            json_encode($options)
+        );
+
+        $this->guardResponse($response);
+
+        return json_decode($response->getContent(), true);
+    }
+
+    protected function getEndpoint(array $parameters = [])
+    {
+        $url = "https://tapfiliate.com/api/1.4/conversions/";
+
+        if (!empty($parameters)) {
+            $queryParameters = [];
+
+            foreach ($parameters as $field => $value) {
+                if (is_bool($value)) {
+                    $queryParameters[] = sprintf("%s=%s", $field, $value ? "true" : "false");
+                    continue;
+                }
+
+                $queryParameters[] = sprintf("%s=%s", $field, urlencode($value));
+            }
+
+            $url .= sprintf("?%s", join("&", $queryParameters));
+        }
+
+        return $url;
+    }
+
+    /**
+     * @param Response $response
+     */
+    protected function guardResponse(Response $response)
+    {
+        if (200 !== $response->getStatusCode()) {
+            throw new \RuntimeException(sprintf(
+                    'An HTTP %s error occurred when reaching tapfiliate API : %s',
+                    $response->getStatusCode(),
+                    $response->getContent()
+                )
+            );
+        }
+    }
+}
